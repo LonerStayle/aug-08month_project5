053f524e6f6cf2745bb1f474c93d5f9cb79a9a45
Loner <LonerStayle@github.com>
2025-08-22 17:33:13 +0900

    aasdasd

--- jin_sup/BuildModel.py
+++ jin_sup/BuildModel.py
@@ -10,8 +10,7 @@ class BuildModel(nn.Module):
         if weights is None:
             raise ValueError("weights 값이 None 입니다!")
         
-        if model_type == ModelType.RESNET18:
-            
+        if model_type == ModelType.RESNET18:   
             self.model = M.resnet18(weights)
             self.model.fc = nn.Linear(self.model.fc.in_features, output_feature, bias=True)
 
@@ -52,6 +51,49 @@ class BuildModel(nn.Module):
         self.weights = weights
         self.preprocess = weights.transforms()
 
+    
+    @staticmethod
+    def get_model(model_type:ModelType, weights, output_feature, checkpoint, device):
+
+        if model_type == ModelType.RESNET18:   
+            model = M.resnet18(weights)
+            model.fc = nn.Linear(model.fc.in_features, output_feature, bias=True)
+
+        elif model_type == ModelType.CONVNEXT_TINY:
+            model = M.convnext_tiny(weights)
+            in_features = model.classifier[2].in_features
+            model.classifier[2] = nn.Linear(in_features, output_feature, bias=True)
+
+        elif model_type == ModelType.CONVNEXT_SMALL:
+            model = M.convnext_small(M.ConvNeXt_Small_Weights.IMAGENET1K_V1)
+            in_features = model.classifier[2].in_features
+            model.classifier[2] = nn.Linear(in_features, 7, bias=True)
+
+        elif model_type == ModelType.CONVNEXT_BASE:
+            model = M.convnext_base(weights)
+            in_features = model.classifier[2].in_features
+            model.classifier[2] = nn.Linear(in_features, output_feature, bias=True)
+
+        elif model_type == ModelType.CONVNEXT_LARGE:
+            model = M.convnext_large(weights)
+            in_features = model.classifier[2].in_features
+            model.classifier[2] = nn.Linear(in_features, output_feature, bias=True)
+
+        elif model_type == ModelType.VIT_B_16:
+            model = M.vit_b_16(weights)
+            in_features = model.heads.head.in_features
+            model.heads.head = nn.Linear(in_features, output_feature, bias=True)
+
+        elif model_type == ModelType.EFFICIENT_NET_B0:
+            model = M.efficientnet_b0(weights)
+            in_features = model.classifier[1].in_features
+            model.classifier[1] = nn.Linear(in_features, output_feature)
+
+        else: pass 
+
+        model.load_state_dict(checkpoint)
+        model.to(device)
+        return model 
         
 
 
