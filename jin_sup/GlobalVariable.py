d7a3efdfd18c005b1e0f0caa51c953a3b605a987
Loner <LonerStayle@github.com>
2025-08-25 15:31:36 +0900

    add image preprocess

---
+++ jin_sup/GlobalVariable.py
@@ -0,0 +1,14 @@
+
+
+class SoundVariable:
+    HZ = 16000
+    F_MAX = 8000
+    N_MELS = 40
+    FIG_SIZE_W = 10
+    FIG_SIZE_H = 4
+    TOP_DB = 30
+
+class ImageVariable:
+    IMAGE_SIZES = (224,224)
+    MEAN = 0.6226921126314113
+    STD = 0.3423165454990286
